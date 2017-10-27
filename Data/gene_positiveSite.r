9ca1e02a6651c579f308ec09e691c76a24ee85f6
Fei Tan <tanfei2007@gmail.com>
2017-10-27 13:18:20 -0400

    Add files via upload

--- Data/gene_positiveSite.r
+++ Data/gene_positiveSite.r
@@ -1,5 +1,5 @@
 
-rm(list = ls)
+rm(list = ls())
 
 gene_posSite<- function(input_file, output_file){
   bed_file = read.table(input_file, sep = '\t')
@@ -7,6 +7,7 @@ gene_posSite<- function(input_file, output_file){
   posSite[,2] = posSite[,2] + 10
   colnames(posSite) = c('chr', 'coordinate', 'strand', 'IPD_ratio', 'methy_level', 'low_95', 'up_95')
   write.csv(file = output_file, posSite, row.names = F, quote = F)
+}
 
 
 # for D. melanogaster
