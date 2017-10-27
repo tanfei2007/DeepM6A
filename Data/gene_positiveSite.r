cca333daf516e8ca35b68d8083f4e9820c65310c
Fei Tan <tanfei2007@gmail.com>
2017-10-27 12:23:20 -0400

    Update gene_positiveSite.r

--- Data/gene_positiveSite.r
+++ Data/gene_positiveSite.r
@@ -6,3 +6,11 @@ gene_posSite<- function(input_file, output_file){
   posSite = bed_file[,-c(3,5)]
   posSite[,2] = posSite[,2] + 10
   colnames(posSite) = c('chr', 'coordinate', 'strand', 'IPD_ratio', 'methy_level', 'low_95', 'up_95')
+  write.csv(file = output_file, posSite, row.names = F, quote = F)
+
+
+# for D. melanogaster
+gene_posSite()  
+  
+  
+  
