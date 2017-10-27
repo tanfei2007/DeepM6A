bbeee9b4e799109db34558a50a1b05b860c10a0b
Fei Tan <tanfei2007@gmail.com>
2017-10-27 12:20:43 -0400

    Update gene_positiveSite.r

--- Data/gene_positiveSite.r
+++ Data/gene_positiveSite.r
@@ -4,4 +4,5 @@ rm(list = ls)
 gene_posSite<- function(input_file, output_file){
   bed_file = read.table(input_file, sep = '\t')
   posSite = bed_file[,-c(3,5)]
-  
+  posSite[,2] = posSite[,2] + 10
+  colnames(posSite) = c('chr', 'coordinate', 'strand', 'IPD_ratio', 'methy_level', 'low_95', 'up_95')
